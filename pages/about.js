50f31887c2ba70c060eb7ebe52702cc0c9b86fc5
mustafa <mustafa.sadriwala@utdallas.edu>
2019-01-04 15:04:50 -0600

    fullscreen video on home page

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import { Container } from 'reactstrap';
 
 export default () => {
   return (
-    <Layout title="about" pageName='About Us'>
+    <Layout title="about" pageName='Our Mission' intro='What are we trying to do here?'>
       <Container>
         <p>THis is the about page</p>
       </Container>
