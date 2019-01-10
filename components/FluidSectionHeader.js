96b65d30ff929f5073c7284ea84c39afa2959919
mustafa <mustafa.sadriwala@utdallas.edu>
2019-01-10 11:15:16 -0600

    Meet Our Team section added to Home

--- components/FluidSectionHeader.js
+++ components/FluidSectionHeader.js
@@ -9,10 +9,12 @@ export default class FluidSectionHeader extends Component {
   render() {
     var string = ''
     this.props.banner ? string = 'linear-gradient(to right, rgba(253, 153, 23, 0.5), rgba(29,178,75, 0.4)), url(' + this.props.banner + ')' : 'url("../static/banner.jpg")'
-    var divStyle = {backgroundImage: string };
+    var divStyle = {backgroundImage: string};
+    var classstring = 'fluid-section-header'
+    if(this.props.className){classstring = this.props.className}
 
     return (
-      <Container className='fluid-section-header' fluid style={divStyle}>
+      <Container fluid style={divStyle} className={classstring}>
         <Container>
           <Row className=''>
             <Col sm='6'>
--- pages/index.js
+++ pages/index.js
@@ -9,15 +9,17 @@ import ReactPlayer from 'react-player';
 
 export default class Index extends Component {
   static async getInitialProps() {
-    const res = await fetch('https://utdcometmarketing-api.herokuapp.com/projects?featured=true&_sort=createdAt:ASC')
+    const res = await fetch('https://utdcometmarketing-api.herokuapp.com/projects?featured=true&_limit=3&_sort=createdAt:ASC')
     let projects = await res.json()
-    projects = projects.slice(0,3)
-    return { projects }
+    const res2 = await fetch('https://utdcometmarketing-api.herokuapp.com/members?_limit=3&_sort=gradyear:ASC')
+    let members = await res2.json()
+    return { projects, members }
   }
 
   componentWillMount() {
     this.setState({
-      posts: this.props.projects
+      projects: this.props.projects,
+      members: this.props.members
     })
   }
 
@@ -57,6 +59,28 @@ export default class Index extends Component {
               ))}
               <Col sm='12' className='d-flex justify-content-center'>
                 <a className='btn-call-to-action btn-call-to-action-dark' href='/portfolio'>View all</a>
+              </Col>
+            </Row>
+          </div>
+        </Container>
+        <FluidSectionHeader text='Meet Our Team' className='team-header'></FluidSectionHeader>
+        <Container>
+          <div className='meet-our-team'>
+            <Row className='recent-projects-row'>
+              {this.props.members.map((person) => (
+                <Col className='person-listing' key={person.id}>
+                  <DynamicLink displayRoute='people' actualRoute='person' slug={person.slug}>
+                    {!!person.profilepicture &&
+                        <img className='img-fluid' src={person.profilepicture.url}></img>
+                    }
+                      <h2 className='heading'>{person.name}</h2>
+                      <p className='lead'>{person.role}</p>
+                      <p className='content'>{person.bio.substring(0, 100) + '...'}</p>
+                  </DynamicLink>
+                </Col>
+              ))}
+              <Col sm='12' className='d-flex justify-content-center'>
+                <a className='btn-call-to-action btn-call-to-action-dark' href='/people'>View all</a>
               </Col>
             </Row>
           </div>
