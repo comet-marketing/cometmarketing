1c3dc4bf55c1e57bf2501e80a6c4abe2f42f11e2
mustafa <mustafa.sadriwala@utdallas.edu>
2019-01-11 00:39:32 -0600

    prototype services grid added to home

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,71 @@ export default class Index extends Component {
   render() {
     return (    
       <Layout title='Home' pageName='Comet Marketing' isHome={true}>
+        <Container className='services-grid'>
+          <Row className='grid-row'>
+            <Col sm='6' className='grid-square'>
+              <Row>
+                <Col>
+                  <h1 className='title'>Photo and Video</h1>
+                  <ul>
+                    <li><h5>We can help cover large or small events with our amazing photographers and/or videographers.</h5></li>
+                    <li><h5>After an editing period, we'll provide you with high-quality, edited photos and/or videos.</h5></li>
+                    <li><h5>A great way to make a legacy for the future of your club.</h5></li>
+                  </ul>
+                </Col>
+                <Col>
+                  <img height='100%' width='100%' src='https://source.unsplash.com/random/400x600/'></img>
+                </Col>
+              </Row>
+            </Col>
+            <Col sm='6' className='grid-square'>
+              <Row>
+                <Col>
+                  <h1 className='title'>Graphic Design</h1>
+                  <ul>
+                    <li><h5>Our team of designers can help inspire new designs and ideas to advertise your upcoming events!</h5></li>
+                    <li><h5>With custom flyers and posters to put around campus and online your event will be as bustling as ever.</h5></li>
+                  </ul>
+                </Col>
+                <Col>
+                  <img height='100%' width='100%' src='https://source.unsplash.com/random/400x600/'></img>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <Row className='grid-row'>
+            <Col sm='6' className='grid-square'>
+            <Row>
+                <Col>
+                  <h1 className="title">Web Design</h1>
+                  <ul>
+                    <li><h5><span>&#191;</span>Like our website?</h5></li>
+                    <li><h5>We can help you make a new or update an old website to cater to your clubs needs on the internet.</h5></li>
+                    <li><h5>Having an online presence will help to keep your organization relevant in the 21st century.</h5></li>
+                  </ul>
+                </Col>
+                <Col>
+                  <img height='100%' width='100%' src='https://source.unsplash.com/random/400x600/'></img>
+                </Col>
+              </Row>
+            </Col>
+            <Col sm='6' className='grid-square'>
+            <Row>
+                <Col>
+                  <h1 className='title'>Social Media</h1>
+                  <ul>
+                    <li><h5>Running social media and understanding its inner workings is difficult.</h5></li>
+                    <li><h5>We gotchu fam, our pros can help you manage your social media account.</h5></li>
+                    <li><h5>With equal parts edginess and relatability we'll help keep your organization on the rise ;)</h5></li>
+                  </ul>
+                </Col>
+                <Col>
+                  <img height='100%' width='100%' src='https://source.unsplash.com/random/400x600/'></img>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
         <Container>
           <Row className='home-video justify-content-center'>
             <div className='player-wrapper'>
