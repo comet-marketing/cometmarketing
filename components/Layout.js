cffa08873bb7b5ccabfa26d13cedc33ee0186239
Al Madireddy <al.madireddy@gmail.com>
2019-01-12 20:11:43 -0600

    responsive grid

--- components/Layout.js
+++ components/Layout.js
@@ -12,6 +12,7 @@ export default class Layout extends Component {
       <div className='layout'>
         <Head>
           <title>{this.props.title}</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
           <link href="https://fonts.googleapis.com/css?family=Merriweather:300,300i,400" rel="stylesheet"></link>
         </Head>
         <div className='layout-header'>
