427958681731234dd6232a1c1f959fce763d2267
mustafa <mustafa.sadriwala@utdallas.edu>
2019-04-01 18:54:05 -0500

    added filtration to people page'

--- pages/people.js
+++ pages/people.js
@@ -11,7 +11,31 @@ import DynamicLink from "../components/DynamicLink";
 import CallToAction from "../components/CallToAction";
 import LazyLoad from "react-lazy-load";
 
+const filterOptions = [
+  { value: 'all', label: 'All Members'},
+  { value: 'exec', label: 'Executive Members' },
+  { value: 'gen', label: 'General Members' },
+  { value: 'photo', label: 'Photo Team' },
+  { value: 'design', label: 'Design Team' },
+  { value: 'video', label: 'Video Team' },
+  { value: 'admin', label: 'Administrative' },
+  { value: 'creative', label: 'Creative' }
+];
+
 export default class People extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      filter: 'all',
+      projects: this.props.projects,
+      chunkedprojects: this.props.chunkedprojects,
+    };
+    this.onFilterSelect = this.onFilterSelect.bind(this);
+    this.filter = this.filter.bind(this);
+  }
+
+
   static chunk(array, size) {
     const chunked_arr = [];
     for (let i = 0; i < array.length; i++) {
@@ -27,15 +51,75 @@ export default class People extends Component {
 
   static async getInitialProps() {
     const res = await fetch('https://utdcometmarketing-api.herokuapp.com/members?_sort=name')
-    let people = await res.json()
-    people = this.chunk(people, 3)
-    return { people }
+    let unchunkedPeople = await res.json()
+    let people = this.chunk(unchunkedPeople, 3)
+    return { people, unchunkedPeople }
   }
 
   componentWillMount() {
     this.setState({
-      posts: this.props.people
+      people: this.props.people,
+      unchunkedPeople: this.props.unchunkedPeople
     })
+  }
+
+  onFilterSelect(e) {
+    this.setState({ filter: e.target.value })
+    this.filter(e.target.value)
+  }
+
+  filter(state) {
+    console.log(state)
+    let newPeople = this.props.unchunkedPeople
+    if(state == 'exec')
+    {
+      newPeople = newPeople.filter(function(element) {
+        return element.Exec
+      })
+    }
+    else if(state == 'gen')
+    {
+      newPeople = newPeople.filter(function(element) {
+        return !element.Exec
+      })
+    }
+    else if(state == 'photo')
+    {
+      newPeople = newPeople.filter(function(element){
+        var str = element.role.toString().toLowerCase()
+        return (element.Exec && str != 'president' && !str.includes('web')) || str.includes('photo')
+      })
+    }
+    else if(state == 'design')
+    {
+      newPeople = newPeople.filter(function(element){
+        var str = element.role.toString().toLowerCase()
+        return (str.includes('design') && !str.includes('web')) || str.includes('creative')
+      })
+    }
+    else if(state == 'video')
+    {
+      newPeople = newPeople.filter(function(element){
+        var str = element.role.toString().toLowerCase()
+        return str.includes('video') || str.includes('sound') || str.includes('creative')
+      })
+    }
+    else if(state == 'admin')
+    {
+      newPeople = newPeople.filter(function(element){
+        var str = element.role.toString().toLowerCase()
+        return !(str.includes('design') || str.includes('creat') || str.includes('vid') || str.includes('sound') || str.includes('pho'))
+      })
+    }
+    else if(state == 'creative')
+    {
+      newPeople = newPeople.filter(function(element){
+        var str = element.role.toString().toLowerCase()
+        return str.includes('design') || str.includes('crea') || str.includes('vid') || str.includes('sound') || str.includes('pho')
+      })
+    }
+
+    this.setState({people: this.constructor.chunk(newPeople, 3)})
   }
 
 /*
@@ -52,7 +136,19 @@ export default class People extends Component {
     return(
       <Layout title='People' pageName='People' intro='This Is Us' banner='/static/Group_pic2_optimized.jpg' description='Meet the talented UTD students behind Comet Marketing' keywords='comet marketing,Comet Marketing,UTD,this is us,people,members' author='Al Madireddy,Mustafa Sadriwala'>
         <Container>
-          {this.props.people.map((row, i) => (
+          <Row className = 'row-no-margin'>
+            <Col sm={{size:3, order: 2, offset: 9}}>
+              <select
+                value={this.state.filter}
+                onChange={this.onFilterSelect}
+                className='filter'>
+                {filterOptions.map((option, i) => (
+                  <option key={i} value={option.value}>{option.label}</option>
+                ))}
+              </select>
+              </Col>
+          </Row>
+          {this.state.people.map((row, i) => (
             <Row key={i} className='row-no-margin'>
               {row.map((person) => (
                 <Col className='person-listing' sm="4" key={person.id}>
