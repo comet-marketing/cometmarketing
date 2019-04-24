ec754d9325793767fe37701a5c6ee54259892f1c
Mustafa Sadriwala <sadri_mustafa@yahoo.com>
2019-04-24 00:22:23 -0500

    join- and leaveDate added to Alumni Page

--- pages/alumni.js
+++ pages/alumni.js
@@ -5,6 +5,11 @@ import fetch from "node-fetch";
 import DynamicLink from "../components/DynamicLink"
 
 export default class Alumni extends Component {
+    constructor(props){
+        super(props);
+        this.getDate = this.getDate.bind(this);
+      }
+
     static async getInitialProps() {
         const res = await fetch('https://utdcometmarketing-api.herokuapp.com/members?Alum=true&_sort=gradyear:DESC')
         let people = await res.json()
@@ -24,26 +29,39 @@ export default class Alumni extends Component {
         return chunked_arr;
     }
 
+    getDate(person) {
+        var join = new Date(person.joinDate)
+        var leave = new Date(person.leaveDate)
+        var months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+        var string = months[(join.getMonth())+1%11] + " " + join.getFullYear() + " - " + months[(leave.getMonth()+1)%11] + " " + leave.getFullYear()
+        return string
+      }
+
     render () {
     return (
         <Layout title='Alumni - UTD Comet Marketing' pageName='Alumni' intro='This Was Us' banner='/static/Group_pic.jpg' description='These are the talented people that helped make Comet Marketing a reality. Their hardwork and dedication has brought us to where we are today.' keywords='comet marketing,Comet Marketing,CM,UTD,this was us,people,members,alumni' author='Mustafa Sadriwala'>
         <Container>
-            <Row>
-            <Col sm='8'>
+            <Row className='row-no-margin'>
+                <Col sm='8'>
                 <p className='lead'>Alumni:</p>
-                {this.props.people.map((person) => (
-                <DynamicLink key={person.id} displayRoute='people' actualRoute='person' slug={person.slug}>
-                    <h2 className='lead'>{person.name}</h2>
-                </DynamicLink>
-                ))}
-            </Col>
-            <Col sm='4' className='justify-text-right'>
-            <p className='lead'>Graduated UTD:</p>
-            {this.props.people.map((person) => (
-                <h3 className='lead'>{person.gradyear}</h3>
-            ))}
-            </Col>
+                </Col>
+                <Col sm='4'>
+                <p className='lead'>Active Time Period:</p>
+                </Col>
             </Row>
+            {this.props.people.map((person) => (
+            <Row className='row-no-margin'>
+                <Col sm='8'>
+                    <DynamicLink key={person.id} displayRoute='people' actualRoute='person' slug={person.slug}>
+                        <h2 className='lead'>{person.name}</h2>
+                    </DynamicLink>
+                </Col>
+                <Col sm='4' className='justify-text-right'>
+                    <h3 className='lead'>{this.getDate(person)}</h3>
+                </Col>
+            </Row>
+            ))}
         </Container>
         </Layout>    
     )
