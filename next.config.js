f9adb68263c3243481642734ed47b96e63f85da2
Al Madireddy <al.madireddy@gmail.com>
2019-08-03 16:05:52 -0400

    blog page + upgrade dependencies

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
-const {PHASE_PRODUCTION_SERVER} = require('next-server/constants')
-module.exports = (phase, {defaultConfig}) => {
-  if (phase === PHASE_PRODUCTION_SERVER) {
-    return {
-      /* production only config */
-    }
-  }
+module.exports = () => {
   const withSass = require('@zeit/next-sass')
   return withSass({})
 }
